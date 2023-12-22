a9b95a157b0ecb9147d6ec5dc90feac2c47c74fa
majinliang <1850455735@qq.com>
2023-12-22 22:15:24 +0800

    添加了导航条

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-window.onload = function () {
-    document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+function showConfirmation() {
+    // 弹出密码确认窗口
+    var password = prompt("请输入密码:");
+
+    // 假设的正确密码
+    var correctPassword = "230118";
+
+    // 验证密码
+    if (password === correctPassword) {
+        // 密码正确，跳转到链接指定的地址
+        window.location.href = document.getElementById("myLink").href;
+    } else {
+        // 密码不正确，给出提示
+        alert("密码不正确，请重新输入");
+    }
 }
