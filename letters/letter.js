92e5cb63c8bcc8b0d87f58fad5ad39a45ca83e18
majinliang <1850455735@qq.com>
2023-12-22 21:35:13 +0800

    添加了密码功能

--- letters/letter.js
+++ letters/letter.js
@@ -1,10 +1,7 @@
-// 在 DOMContentLoaded 事件中封装代码，确保 DOM 已完全加载
 document.addEventListener("DOMContentLoaded", function () {
-    // 检查 ID 为 "mainDisplay "的元素是否存在
     var mainDisplay = document.getElementById("mainDisplay");
 
     if (mainDisplay !== null) {
-        // 检查用户是否使用移动设备
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             console.log("on mobile side");
             mainDisplay.classList.add("onMobileFont");
