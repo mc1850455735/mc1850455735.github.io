496ba7194bde3e6a719ead78034f35e7f8e800bf
majinliang <1850455735@qq.com>
2023-12-22 17:56:02 +0800

    移动端和电脑端对情书的区别显示

--- letters/letter.js
+++ letters/letter.js
@@ -1,10 +1,18 @@
-var myElement = document.getElementById("mainDisplay");
+// 在 DOMContentLoaded 事件中封装代码，确保 DOM 已完全加载
+document.addEventListener("DOMContentLoaded", function () {
+    // 检查 ID 为 "mainDisplay "的元素是否存在
+    var mainDisplay = document.getElementById("mainDisplay");
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    console.log("on moblie side");
-    myElement.classList.remove("onPCFont");
-    myElement.classList.add("onMobileFont");
-} else {
-    console.log("on pc side");
-}
-
+    if (mainDisplay !== null) {
+        // 检查用户是否使用移动设备
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            console.log("on mobile side");
+            mainDisplay.classList.add("onMobileFont");
+            mainDisplay.classList.remove("onPCFont");
+        } else {
+            console.log("on PC side");
+        }
+    } else {
+        console.error("Element with ID 'mainDisplay' not found");
+    }
+});
