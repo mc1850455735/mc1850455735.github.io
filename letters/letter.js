eddd6253cba2a36a6c5b448296bf4b202481ab4d
majinliang <1850455735@qq.com>
2023-12-22 23:40:10 +0800

    照片墙和更好的文本显示

--- letters/letter.js
+++ letters/letter.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var mainDisplay = document.getElementById("mainDisplay");
+    var preDisplay = document.getElementById("preDisplay")
 
     if (mainDisplay !== null) {
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             console.log("on mobile side");
-            mainDisplay.classList.add("onMobileFont");
-            mainDisplay.classList.remove("onPCFont");
+            preDisplay.classList.add("onMobileFont");
         } else {
             console.log("on PC side");
+            preDisplay.classList.add("onPCFont");
         }
     } else {
         console.error("Element with ID 'mainDisplay' not found");
