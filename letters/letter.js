f705d5825614011ee205bcc2bd14f52877bce9f3
majinliang <1850455735@qq.com>
2024-01-01 22:26:51 +0800

    手机端的隐藏式菜单

--- letters/letter.js
+++ letters/letter.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     var preDisplay = document.getElementById("preDisplay");
     var sideMenu = document.getElementById("sideMenu");
+    var addMenu = document.getElementById("addMenu");
 
     if (mainDisplay !== null) {
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             console.log("on mobile side");
             preDisplay.classList.add("onMobileFont");
             sideMenu.style.display = "none";
+            addMenu.style.display = "block";
         } else {
             console.log("on PC side");
             preDisplay.classList.add("onPCFont");
@@ -33,4 +35,14 @@ function changeShowLetter(n) {
     for (var i = 0; i < children.length; i++) {
         children[i].style.display = "flow";
     }
+}
+
+function showmoreletter(){
+    var sideMenu = document.getElementById("sideMenu");
+
+    if(sideMenu.style.display === "none")
+        sideMenu.style.display = "block";
+    else 
+        sideMenu.style.display = "none";
+
 }
