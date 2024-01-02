cb2fe33d93a5fa626cfee5aee12c72ee17eb8a05
majinliang <1850455735@qq.com>
2024-01-02 17:53:41 +0800

    测试

--- letters/letter.js
+++ letters/letter.js
@@ -1,10 +1,18 @@
+function onPC(){
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     var preDisplay = document.getElementById("preDisplay");
     var sideMenu = document.getElementById("sideMenu");
     var addMenu = document.getElementById("addMenu");
 
     if (mainDisplay !== null) {
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        if (!onPC()) {
             console.log("on mobile side");
             preDisplay.classList.add("onMobileFont");
             sideMenu.style.display = "none";
@@ -23,18 +31,30 @@ function changeShowLetter(n) {
     var showLetter = document.getElementById(target);
 
     var container = document.getElementById("mainDisplay");
-    var children = container.querySelectorAll("*"); 
-    for (var i = 0; i < children.length; i++) {
-        children[i].style.display = "none";
-    }
 
-    console.log(children.length);
+    if(onPC()){
+        var children = container.querySelectorAll("*"); 
+        for (var i = 0; i < children.length; i++) {
+            children[i].style.display = "none";
+        }
 
-    showLetter.style.display = "block";
-    var children = showLetter.querySelectorAll("*"); 
-    for (var i = 0; i < children.length; i++) {
-        children[i].style.display = "flow";
+        console.log(children.length);
+
+        showLetter.style.display = "block";
+        var children = showLetter.querySelectorAll("*"); 
+        for (var i = 0; i < children.length; i++) {
+            children[i].style.display = "flow";
+        }
     }
+    else{
+        var children = container.querySelectorAll("*"); 
+        for (var i = 0; i < children.length; i++) {
+            children[i].style.display = "none";
+        }
+
+        showLetter.style.display = "block";
+    }
+    
 }
 
 function showmoreletter(){
